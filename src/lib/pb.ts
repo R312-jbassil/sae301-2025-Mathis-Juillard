7a36d221ca90ed4ce709cf15bcf34b264ed0f318
hmabed <hmabed@gmail.com>
2025-12-01 14:13:29 +0100

    Hakim Mabed changes

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -3,7 +3,7 @@ type TypedPocketBase = PocketBase;
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'http://localhost:8091'   //localhost = machine de déploiement
+else path = 'https://sae-301.mathis-juillard.fr:443'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
 
